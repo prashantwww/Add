e1b4452efdcbb74cf3b41c0c06c1f0ae2f2bef02
vuiwks01 <vuiwks01@VUIWKS01.lko.jconnect.in>
2016-04-04 14:16:14 +0530

    test commit

---
+++ src/add/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package add;
+
+/**
+ *
+ * @author Prashant
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
